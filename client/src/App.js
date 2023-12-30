ba4ebdf65f2bfd28faad88a42451b32943f655ab
bryanlzl <zhenlun.bryan@gmail.com>
2023-12-30 15:51:03 +0800

    change: remove redudant components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { TestComponent } from "./components/TestComponent/TestComponent";
 import TaskTracker from "./components/TaskTracker/taskTracker";
 import styles from "./styles/App.module.css";
 import "./styles/font.css";
@@ -9,10 +8,6 @@ function App() {
 
   return (
     <div className={styles.App}>
-      <button onClick={() => setHidePractice(!hidePractice)}>
-        {hidePractice ? "Show Practice" : "Hide Practice"}
-      </button>
-      {!hidePractice && <TestComponent />}
       <TaskTracker />
     </div>
   );
